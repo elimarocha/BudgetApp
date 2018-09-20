b253cf720ebc2c8ed731b4ccffcf926809e5982e
Jesus Fragoso <tonyfra1997@gmail.com>
2018-09-20 12:01:04 -0400

    ttest1

---
+++ build/Pods.build/Debug-iphoneos/Pods-BillApp.build/DerivedSources/Pods_BillApp_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_BillAppVersionString[];
+ extern const double Pods_BillAppVersionNumber;
+
+ const unsigned char Pods_BillAppVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_BillApp  PROJECT:Pods-1" "\n";
+ const double Pods_BillAppVersionNumber __attribute__ ((used)) = (double)1.;
